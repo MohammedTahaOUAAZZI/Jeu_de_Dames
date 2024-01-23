1f553729c9441edcec002706f91fd38faff7c356
ouaazzi <taha.ouaazzi@gmail.com>
2024-01-23 23:22:43 +0100

    Premiere code du Jeu de Damme

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+WIDTH, HEIGHT = 700, 700
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+
+#RDG colors
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
