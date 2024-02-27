6049ab0d9936ec1ed3fe151a2d2ff1ab067de460
Houssam <houssamnajih13@gmail.com>
2024-02-28 00:01:24 +0100

    modification

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -13,9 +13,28 @@ def minimax(position, depth, max_player, game):
         best_move = None
         for move in get_all_moves(position, WHITE, game):
 
-
-
     else:
+        
 
+
+
+def simulate_move(piece, move, board, game, skip):
+    board.move(piece, move[0], move[1])
+    if skip:
+        board.remove(skip)
+
+    return board
 
 def get_all_moves(position, color, game):
+
+    moves = []
+
+    for piece in board.get_all_pieces(color):
+        valid_moves = board.get_valid_moves(piece)
+        for move, skip in valid_moves.items():
+            temp_board = deepcopy(board)
+            new_board = simulate_move(piece, move, temp_board, game, skip)
+            moves.append([new_board, piece])
+    
+    return moves
+
