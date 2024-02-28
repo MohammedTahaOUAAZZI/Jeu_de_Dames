53ac466366090ec6a5eaa5ff59cad1a0b387793c
Houssam <houssamnajih13@gmail.com>
2024-02-28 02:14:39 +0100

    full code

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -12,8 +12,23 @@ def minimax(position, depth, max_player, game):
         maxEval = float('-inf')
         best_move = None
         for move in get_all_moves(position, WHITE, game):
+            evaluation = minimax(move, depth-1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+        
+        return maxEval, best_move
 
     else:
+        minEval = float('inf')
+        best_move = None
+        for move in get_all_moves(position, RED, game):
+            evaluation = minimax(move, depth-1, True, game)[0]
+            minEval = min(minEval, evaluation)
+            if minEval == evaluation:
+                best_move = move
+        
+        return minEval, best_move
         
 
 
@@ -32,9 +47,19 @@ def get_all_moves(position, color, game):
     for piece in board.get_all_pieces(color):
         valid_moves = board.get_valid_moves(piece)
         for move, skip in valid_moves.items():
+            draw_moves(game, board, piece)
             temp_board = deepcopy(board)
-            new_board = simulate_move(piece, move, temp_board, game, skip)
-            moves.append([new_board, piece])
+            temp_piece = temp_board.get_piece(piece.row, piece.col)
+            new_board = simulate_move(temp_piece, move, temp_board, game, skip)
+            moves.append(new_board)
     
     return moves
 
+def draw_moves(game, board, piece):
+
+    valid_moves = board.get_valid_moves(piece)
+    board.draw(game.win)
+    pygame.draw.circle(game.win, (0,255,0), (piece.x, piece.y), 50, 5)
+    game.draw_valid_moves(valid_moves.keys())
+    pygame.display.update()
+    #pygame.time.delay(100)
